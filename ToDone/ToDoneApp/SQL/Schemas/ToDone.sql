61229600d52daf231da44687717c90ef0c63ba52
Christian Berard <chrberard@ksu.edu>
2022-11-14 19:48:32 -0600

    Initial PUSH

---
+++ ToDone/ToDoneApp/SQL/Schemas/ToDone.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS
+(
+	SELECT *
+	FROM sys.schemas s
+	WHERE s.[name] = N'ToDone'
+)
+BEGIN
+	EXEC(N'CREATE SCHEMA [ToDone] AUTHORIZATION [dbo]');
+END;
---
+++ ToDone/ToDoneApp/SQL/Tables/DropTables.sql
@@ -0,0 +1,7 @@
+﻿Drop Table If Exists ToDone.GroupUser;
+Drop Table If Exists ToDone.Groups;
+Drop Table If Exists ToDone.TaskComment;
+Drop Table If Exists ToDone.Task;
+Drop Table If Exists ToDone.PrivacyLevel;
+Drop Table If Exists ToDone.FriendsUser;
+Drop Table If Exists ToDone.Users;
