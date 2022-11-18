e9926fa69d7da5a73d7a93f51fef98910571b979
Christian Berard <chrberard@ksu.edu>
2022-11-17 18:40:28 -0600

    Push for merge

---
+++ ToDone/ToDoneApp/SQL/Procedures/ToDone.FetchUsersGroups.sql
@@ -0,0 +1,7 @@
+﻿CREATE OR ALTER PROCEDURE ToDone.FetchUsersGroups
+	@UserID INT
+AS
+SELECT G.GroupID, G.Title, G.[Description]
+FROM ToDone.Groups G
+INNER JOIN ToDone.GroupUser GU ON GU.UserID = @UserID
+GO
